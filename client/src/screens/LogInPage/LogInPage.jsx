17b499a92d88cdc9a67f7325487d3b86ff876c1c
Zulay S <zulays@Zulays-MacBook-Pro.local>
2020-07-17 15:15:37 -0400

    working on header styling. trying to fix importing on log in page. commented out manageprofile component in app.jsx since it does not exist yet

---
+++ client/src/screens/LogInPage/LogInPage.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "./LogInPage.css"
+import Header from "../components/shared/Header/Header"
+import Footer from "../components/shared/Footer/Footer"
+
+
+const LogInPage = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Log In</h1>
+        <p>Don’t have an account? <span className="register">Register</span> today!</p>
+        <form>
+          <label>Username or Email Address</label>
+          <input type="text" className="inputs" />
+          <label>Password</label>
+          <input type="password" className=" inputs" />
+          <p className="forgot-pass">Forgot password?</p>
+          <input type="checkbox" className="checkbox" /> <label className="checkbox-label">Remember Me</label>
+          <button className=" login-button">Log In</button>
+        </form >
+      </main >
+      <Footer />
+    </div >
+  )
+}
+export default LogInPage
