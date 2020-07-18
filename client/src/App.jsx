78aae578dccf62fd5d2b4066f7ed6804f9adc469
hi <kangja1996@gmail.com>
2020-07-18 15:24:07 -0400

    changed naming convention and added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import LogInPage from "./screens/LogInPage/LogInPage";
-// manageprofile should be uncommented
-import ManageProfile from "./screens/ManageProfile/ManageProfile";
+// import ManageProfile from "./screens/ManageProfile/ManageProfile";
 import UserCreate from "./screens/UserCreate/UserCreate";
 // import UserEdit from "./screens/UserEdit/UserEdit"
 // import UserDelete from "./screens/UserDelete/UserDelete"
@@ -13,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/login" component={LogInPage}></Route>
-      <Route exact path="/manageprofile" component={ManageProfile}></Route>
+      {/* <Route exact path="/manageprofile" component={ManageProfile}></Route> */}
       {/* <Route exact path="/userdelete" component={UserDelete}></Route> */}
       {/* <Route exact path="/useredit" component={UserEdit}></Route>
       <Route exact path="/userhome" component={UserHome}></Route> */}
--- client/src/screens/ManageProfile/ManageProfile.jsx
+++ client/src/screens/ManageProfile/ManageProfile.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./ManageProfile.css";
-import { Redirect } from "react-router-dom";
 import Header from "../../components/shared/Header/Header";
 import Footer from "../../components/shared/Footer/Footer";
+import { Link } from "react-router-dom";
 
 const ManageProfile = () => {
   return (
@@ -11,15 +11,19 @@ const ManageProfile = () => {
       <div className="manageprofile-bigger">
         <h1 className="manageprofile-manage">Manage My Profile</h1>
         <div className="manageprofile-container">
-          <div className="manageprofile-rectangle-38">
-            <p className="manageprofile-edit">Edit My Profile</p>
-          </div>
+          <Link className="manageprofile-link" to={"/useredit"}>
+            <div className="manageprofile-rectangle-38">
+              <p className="manageprofile-edit">Edit My Profile</p>
+            </div>
+          </Link>
           <div className="manageprofile-rectangle-39">
             <p className="manageprofile-hide">Hide my Profile from Others</p>
           </div>
-          <div className="manageprofile-rectangle-40">
-            <p className="manageprofile-delete">Delete my Profile</p>
-          </div>
+          <Link className="manageprofile-link" to={"/userdelete"}>
+            <div className="manageprofile-rectangle-40">
+              <p className="manageprofile-delete">Delete my Profile</p>
+            </div>
+          </Link>
         </div>
       </div>
       <Footer />
