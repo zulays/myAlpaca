b1f92007b864668a7ed93fc3e93c02da12e6c27a
Zulay S <zulays@Zulays-MacBook-Pro.local>
2020-07-18 14:52:52 -0400

    adjusted label alignment in form

--- client/src/screens/LogInPage/LogInPage.jsx
+++ client/src/screens/LogInPage/LogInPage.jsx
@@ -10,15 +10,15 @@ const LogInPage = () => {
       <Header />
       <main>
         <h1>Log In</h1>
-        <p>Don't have an account? <Link to="/usercreate"><span fontWeight="700">Register</span></Link> today!</p>
-        <form>
+        <p>Don't have an account? <Link to="/usercreate" className="register-text"><span className="register-text" fontWeight="700">Register</span></Link> today!</p>
+        <form className="login-form">
           <label>Username or Email Address</label>
           <input type="text" className="inputs" />
           <label>Password</label>
           <input type="password" className="inputs" />
-          <p className="forgot-pass">Forgot password?</p>
+          <label className="forgot-pass">Forgot password?</label>
           <label className="checkbox-inline"><input type="checkbox" value="" className="checkbox" />Remember Me</label>
-          <button className="login-button">Log In</button>
+          <button className="loginpage-button">Log In</button>
         </form>
       </main>
       <Footer />
