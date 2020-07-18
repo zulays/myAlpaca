360e8a26f06ef7aca67c2746cde859701573988b
Zulay S <zulays@Zulays-MacBook-Pro.local>
2020-07-18 14:23:36 -0400

    UserHome component created. Linking to be added next

--- client/src/screens/UserHome/UserHome.jsx
+++ client/src/screens/UserHome/UserHome.jsx
@@ -10,17 +10,35 @@ const UserHome = () => {
       <Header />
       {/* <LogInHeader /> */}
       <main>
-        <h1>Log In</h1>
-        <p>Don't have an account? <span fontWeight="700">Register</span> today!</p>
-        <form>
-          <label>Username or Email Address</label>
-          <input type="text" className="inputs" />
-          <label>Password</label>
-          <input type="password" className="inputs" />
-          <p className="forgot-pass">Forgot password?</p>
-          <input type="checkbox" className="checkbox" /> <label className="checkbox-label">Remember Me</label>
-          <button className="login-button">Log In</button>
-        </form>
+        <table>
+          <tbody>
+            <tr>
+              <td className="local-events">Local Events</td>
+              <td className="weather">Weather</td>
+            </tr>
+            <tr>
+              <td className="restaurants">Restaurants</td>
+              <td className="currency">Currency</td>
+            </tr>
+            <tr>
+              <td className="transportation">Transportation</td>
+              <td className="languages">Languages</td>
+            </tr>
+            <tr>
+              <td className="culture">Culture</td>
+              <td className="shoppping">Shopping</td>
+            </tr>
+          </tbody>
+
+        </table>
+        {/* <div className="local-events">Local Events</div>
+        <div className="weather">Weather</div>
+        <div className="restaurants">Restaurants</div>
+        <div className="currency">Currency</div>
+        <div className="transportation">Transporation</div>
+        <div className="languages">Languages</div>
+        <div className="culture">Culture</div>
+        <div className="shopping">Shopping</div> */}
       </main>
       <Footer />
     </>
