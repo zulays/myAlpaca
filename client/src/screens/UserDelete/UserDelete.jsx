8391f3f7b1c785ea053f0a96ea1bbb82e2b4441a
maliccny <maliccny@gmail.com>
2020-07-19 19:08:50 -0400

    added more css

--- client/src/screens/UserDelete/UserDelete.jsx
+++ client/src/screens/UserDelete/UserDelete.jsx
@@ -21,21 +21,21 @@ class UserDelete extends Component {
     const { user } = this.state
     return (
       <div className="userDelete-section">
-        <p>We are sorry to se yu go...</p>
+        <p className="sorry-title">We are sorry to see you go...</p>
         <div className="options-box">
-          <p>Tell us why so we can get better</p>
+          <p className="tell-us">Tell us why so we can get better</p>
           <div className="checkbox-div">
             <div className="no-longer">
-              <input type="checkbox" id="no-longer" name="no-longer"></input>
-              <label for="no-longer">I'm no longer studying abroad</label>
+              <input className="checkbox" type="checkbox" id="no-longer" name="no-longer"></input>
+              <label htmlFor="no-longer">I'm no longer studying abroad</label>
             </div>
             <div className="not-useful">
-              <input type="checkbox" id="not-useful" name="not-useful"></input>
-              <label for="not-useful">I didn't find the website useful</label>
+              <input className="checkbox" type="checkbox" id="not-useful" name="not-useful"></input>
+              <label htmlFor="not-useful">I didn't find the website useful</label>
             </div>
             <div className="wrong-info">
-              <input type="checkbox" id="wrong-info" name="wrong-info"></input>
-              <label for="wrong-info">Website provided wrong information</label>
+              <input className="checkbox" type="checkbox" id="wrong-info" name="wrong-info"></input>
+              <label htmlFor="wrong-info">Website provided wrong information</label>
             </div>
           </div>
         </div>
