079e645d43259fa4a9f78a8c882867f0414e01be
Zulay S <zulays@Zulays-MacBook-Pro.local>
2020-07-20 10:02:09 -0400

    set state of id in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,28 +7,35 @@ import UserCreate from "./screens/UserCreate/UserCreate";
 // import UserEdit from "./screens/UserEdit/UserEdit"
 import UserDelete from "./screens/UserDelete/UserDelete"
 import UserHome from "./screens/UserHome/UserHome"
+import { getUsers } from "./services/users";
 
 class App extends Component {
-  construtor() {
+  constructor() {
     super()
     this.state = {
-      user = ""
+      id: ""
     }
   }
 
 
   componentDidMount = async () => {
-
+    const users = await getUsers()
+    console.log(users[0])
+    console.log(users[0]._id)
+    this.setState({
+      id: users[0]._id
+    })
   }
 
 
   render() {
     return (
       <div className="App" >
+        <Route exact path="/">Home</Route>
         <Route exact path="/login" component={LogInPage}></Route>
         <Route exact path="/manageprofile" component={ManageProfile}></Route>
-        <Route exact path="/userdelete" component={UserDelete}></Route>
-        {/* <Route exact path="/useredit" component={UserEdit}></Route>*/}
+        <Route exact path="/userdelete" component={UserDelete} id={this.state.id}></Route>
+        {/* <Route exact path="/useredit" component={UserEdit} id={this.state.id}></Route> */}
         <Route exact path="/userhome" component={UserHome} ></Route>
         <Route path="/usercreate" component={UserCreate}></Route>
       </div >
