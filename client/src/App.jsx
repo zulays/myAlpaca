4e6b6f438dde3c51cd4aea3488bcc1e4f3e395fa
hi <kangja1996@gmail.com>
2020-07-20 11:15:45 -0400

    fixed userDelete jsx and app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,40 +5,44 @@ import LogInPage from "./screens/LogInPage/LogInPage";
 import ManageProfile from "./screens/ManageProfile/ManageProfile";
 import UserCreate from "./screens/UserCreate/UserCreate";
 // import UserEdit from "./screens/UserEdit/UserEdit"
-import UserDelete from "./screens/UserDelete/UserDelete"
-import UserHome from "./screens/UserHome/UserHome"
+import UserDelete from "./screens/UserDelete/UserDelete";
+import UserHome from "./screens/UserHome/UserHome";
 import { getUsers } from "./services/users";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      id: ""
-    }
+      user: "",
+    };
   }
-
 
   componentDidMount = async () => {
-    const users = await getUsers()
-    console.log(users[0])
-    console.log(users[0]._id)
+    const users = await getUsers();
+    console.log(users[0]);
+    console.log(users[0]._id);
     this.setState({
-      id: users[0]._id
-    })
-  }
-
+      user: users[0],
+    });
+  };
 
   render() {
     return (
-      <div className="App" >
-        <Route exact path="/">Home</Route>
+      <div className="App">
+        <Route exact path="/">
+          Home
+        </Route>
         <Route exact path="/login" component={LogInPage}></Route>
         <Route exact path="/manageprofile" component={ManageProfile}></Route>
-        <Route exact path="/userdelete" component={UserDelete} id={this.state.id}></Route>
+        <Route
+          exact
+          path="/userdelete"
+          render={() => <UserDelete user={this.state.user} />}
+        ></Route>
         {/* <Route exact path="/useredit" component={UserEdit} id={this.state.id}></Route> */}
-        <Route exact path="/userhome" component={UserHome} ></Route>
+        <Route exact path="/userhome" component={UserHome}></Route>
         <Route path="/usercreate" component={UserCreate}></Route>
-      </div >
+      </div>
     );
   }
 }
