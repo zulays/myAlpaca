142171f07017c7456f847059f317d21065285675
hi <kangja1996@gmail.com>
2020-07-21 12:01:05 -0400

    made Home screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import LogInPage from "./screens/LogInPage/LogInPage";
-// import ManageProfile from "./screens/ManageProfile/ManageProfile";
+import ManageProfile from "./screens/ManageProfile/ManageProfile";
 import UserCreate from "./screens/UserCreate/UserCreate";
 import UserEdit from "./screens/UserEdit/UserEdit";
 import UserDelete from "./screens/UserDelete/UserDelete";
 import UserHome from "./screens/UserHome/UserHome";
 import { getUsers } from "./services/users";
-
-// delete
-import Home from "./screens/Home/Home";
-// delete
 
 class App extends Component {
   constructor() {
@@ -31,17 +27,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* delete */}
-        <Route exact path="/" component={Home}>
-          Home
-        </Route>
-        {/*  */}
-
         <Route exact path="/">
           Home
         </Route>
         <Route exact path="/login" component={LogInPage}></Route>
-        {/* <Route exact path="/manageprofile" component={ManageProfile}></Route> */}
+        <Route exact path="/manageprofile" component={ManageProfile}></Route>
         {this.state.user && (
           <Route
             exact
