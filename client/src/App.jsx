1aa52a4034e63c418d8e1a1a86ec7f3fbd4cd172
hi <kangja1996@gmail.com>
2020-07-21 11:06:35 -0400

    in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import LogInPage from "./screens/LogInPage/LogInPage";
-import ManageProfile from "./screens/ManageProfile/ManageProfile";
+// import ManageProfile from "./screens/ManageProfile/ManageProfile";
 import UserCreate from "./screens/UserCreate/UserCreate";
-import UserEdit from "./screens/UserEdit/UserEdit"
+import UserEdit from "./screens/UserEdit/UserEdit";
 import UserDelete from "./screens/UserDelete/UserDelete";
 import UserHome from "./screens/UserHome/UserHome";
 import { getUsers } from "./services/users";
+
+// delete
+import Home from "./screens/Home/Home";
+// delete
 
 class App extends Component {
   constructor() {
@@ -27,11 +31,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/">Home</Route>
+        {/* delete */}
+        <Route exact path="/" component={Home}>
+          Home
+        </Route>
+        {/*  */}
+
+        <Route exact path="/">
+          Home
+        </Route>
         <Route exact path="/login" component={LogInPage}></Route>
-        <Route exact path="/manageprofile" component={ManageProfile}></Route>
-        {this.state.user && <Route exact path="/userdelete" render={() => <UserDelete user={this.state.user} />}></Route>}
-        {this.state.user && <Route exact path="/useredit" render={() => <UserEdit user={this.state.user} />}></Route>}
+        {/* <Route exact path="/manageprofile" component={ManageProfile}></Route> */}
+        {this.state.user && (
+          <Route
+            exact
+            path="/userdelete"
+            render={() => <UserDelete user={this.state.user} />}
+          ></Route>
+        )}
+        {this.state.user && (
+          <Route
+            exact
+            path="/useredit"
+            render={() => <UserEdit user={this.state.user} />}
+          ></Route>
+        )}
         <Route exact path="/userhome" component={UserHome}></Route>
         <Route path="/usercreate" component={UserCreate}></Route>
       </div>
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Home.css";
+import Header from "../../components/shared/Header/Header";
+import Footer from "../../components/shared/Footer/Footer";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default ManageProfile;
