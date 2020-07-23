3faea8b2d80946b5573630851ba3f4dddaa77bdd
Zulay S <zulays@Zulays-MacBook-Pro.local>
2020-07-23 16:26:03 -0400

    updates made to userHome. not fully complete yet

--- client/src/screens/UserHome/UserHome.jsx
+++ client/src/screens/UserHome/UserHome.jsx
@@ -9,28 +9,24 @@ const UserHome = () => {
   return (
     <>
       <LogInHeader />
-      <main>
-        <WelcomeBanner />
-        <table className="activities-chart">
-          <tbody>
-            <tr className="rows">
-              <td className="local-events">Local Events</td>
-              <td className="weather">Weather</td>
-            </tr>
-            <tr className="rows">
-              <td className="restaurants">Restaurants</td>
-              <td className="currency">Currency</td>
-            </tr>
-            <tr className="rows">
-              <td className="transportation">Transportation</td>
-              <td className="languages">Languages</td>
-            </tr>
-            <tr className="rows">
-              <td className="culture">Culture</td>
-              <td className="shopping">Shopping</td>
-            </tr>
-          </tbody>
-        </table>
+      <WelcomeBanner />
+      <main className="activities-chart">
+        <div className="row">
+          <div className="local-events"><h1 className="image-text">Local Events</h1></div>
+          <div className="weather"><h1 className="image-text">Weather</h1></div>
+        </div>
+        <div className="row">
+          <div className="restaurants"><h1 className="image-text">Restaurants</h1></div>
+          <div className="currency"><h1 className="image-text">Currency</h1></div>
+        </div>
+        <div className="row">
+          <div className="transportation"><h1 className="image-text">Transporation</h1></div>
+          <div className="languages"><h1 className="image-text">Languages</h1></div>
+        </div>
+        <div className="row">
+          <div className="culture"><h1 className="image-text">Culture</h1></div>
+          <div className="shopping"><h1 className="image-text">Shopping</h1></div>
+        </div>
       </main>
       <Footer />
     </>
